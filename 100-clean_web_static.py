a85064ec227fc18a2d52d37f92425e69d3a2b41f
itsoluwatobby <oluwasamuel050@gmail.com>
2023-12-10 07:05:21 +0100

    m

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,13 +13,13 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    archives_list = sorted(os.listdir("versions"))
+    [archives_list.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("rm ./{}".format(a)) for a in archives_list]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
+        archives_slice = run("ls -tr").split()
+        archives = [a for a in archives_slice if "web_static_" in a]
         [archives.pop() for i in range(number)]
         [run("rm -rf ./{}".format(a)) for a in archives]
