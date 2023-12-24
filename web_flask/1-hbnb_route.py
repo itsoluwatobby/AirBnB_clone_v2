9c43c9ed6f748f4990d06a20e5646b14c54672bc
itsoluwatobby <oluwasamuel050@gmail.com>
2023-12-24 06:47:56 +0100

    syntax correction

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,6 +4,7 @@ a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
     /: display “Hello HBNB!”
+
     /hbnb: display “HBNB”
 """
 from flask import Flask
